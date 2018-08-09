ed933f5237cdf18179a40554e54a76107277b00e
detarso7 <38414640+detarso7@users.noreply.github.com>
2018-08-09 15:33:55 -0300

    Criação da Rota "ordem-compra" e data binding e validação do form

---
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordem-compra',
+  templateUrl: './ordem-compra.component.html',
+  styleUrls: ['./ordem-compra.component.css']
+})
+export class OrdemCompraComponent implements OnInit {
+
+public endereco:string = ""
+public numero:string = ""
+public complemento:string = ""
+public formaPagamento:string = ""
+
+public enderecoValido:boolean
+public numeroValido:boolean
+public complementoValido:boolean
+public selectedValido:boolean
+
+public enderecoPrimitivo:boolean = true
+public numeroPrimitivo:boolean = true
+public complementoPrimitivo:boolean = true
+public selectPrimitivo:boolean = true
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public atualizarEndereco(endereco:string):void{
+    this.endereco = endereco
+   // console.log(endereco)
+
+this.enderecoPrimitivo = false
+
+   if(endereco.length > 3 || undefined){
+     this.enderecoValido = true
+   }else{
+     this.enderecoValido = false
+   }
+  }
+
+
+  public atualizarNumero(numero:string):void{
+    this.numero = numero
+    //console.log(numero)
+    this.numeroPrimitivo = false
+
+    if (numero.length > 0){
+      this.numeroValido = true
+    }else{
+      this.numeroValido = false
+    }
+  }
+
+  public atualizarComplemento(complemento:string):void{
+    this.complemento = complemento
+    //console.log(complemento)
+
+this.complementoPrimitivo = false
+
+    if(complemento.length > 0){
+      this.complementoValido = true
+    }
+  }
+
+  public atualizarSelected(selected:string):void{
+    this.formaPagamento = selected
+    //console.log(selected)
+
+this.selectPrimitivo = false
+
+    if(this.formaPagamento){
+      this.selectedValido = false
+    }else{
+      this.selectedValido = true
+    }
+  }
+
+}
